4d3e4addc9e311b7a41562624cc8ce995ebc691a
eQuiNoX <zubin.mithra@gmail.com>
2012-10-30 20:23:58 +0530

    encodeLocationHeaders function added

--- hoenheim/encoder/Encoder.java
+++ hoenheim/encoder/Encoder.java
@@ -41,4 +41,18 @@ public class Encoder
       }
       return output.toString();
    }
+   
+   public String encodeLocationHeaders(String input)
+   {
+      int i = 0;
+      StringBuilder output = new StringBuilder();
+      
+      for( i = 0; i < input.length(); ++i )
+      {
+         if ( input.charAt(i) == ';' )
+            output.append(Integer.toHexString(input.charAt(i) - '0'));
+      }
+      return output.toString();
+   }
+
 }
