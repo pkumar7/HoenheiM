c8a7d67fdf41a2b9f20fbbf5bcf2ac288ce61a05
eQuiNoX <zubin.mithra@gmail.com>
2012-10-31 23:08:12 +0530

    Encoder.encodeJsInsideString added

--- hoenheim/encoder/Encoder.java
+++ hoenheim/encoder/Encoder.java
@@ -66,5 +66,31 @@ public class Encoder
       */
       return "<![CDATA[" + input + "]]>";
    }
-
+   
+   public String encodeJsInsideString(String input)
+   {
+      /* This function is to be used in scenarios where user
+      input goes/might go inside a quoted string inside a javascript block.
+      Please note that for other kind of scripts, the encoding might vary
+      and that this function is not to be used.
+      For non alphanumerics, entity encode.
+      NOTE : this is for JS inside string in HTML, NOT XHTML.
+      For XHTML, you need to make sure the script block is inside a CDATA!!!
+      */
+      int i = 0;
+      StringBuilder output = new StringBuilder();
+      
+      for( i = 0; i < input.length(); ++i )
+      {
+         if( input.charAt(i) == '<' )
+            output.append("&lt;");
+         else if ( input.charAt(i) == '>' )
+            output.append("&gt;");
+         else if ( input.charAt(i) == '&' )
+            output.append("&amp;");
+         else if ( Character.isLetter(input.charAt(i)) || Character.isDigit(input.charAt(i)) )
+            output.append("&#x" + Integer.toHexString(input.charAt(i) - '0'));
+      }
+      return output.toString();
+   }
 }
